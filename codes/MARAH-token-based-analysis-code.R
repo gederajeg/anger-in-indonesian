a9850e67cc7a492a5cfed356408cb2c1cf418f30
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2022-01-16 15:11:53 +0800

    Add the codes for the salience analysis of metonymy and fix code for the metaphor analysis.

--- codes/MARAH-token-based-analysis-code.R
+++ codes/MARAH-token-based-analysis-code.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
-# read the data
-marah <- read_tsv("data/leipzig-amarah-kemarahan-marah-conc.txt") %>% 
+# read the metaphor data
+marah <- read_tsv("data/token-based-approach-main.txt") %>% 
   filter(USE == "metaphor", !LU_POS %in% c("adj", "prepo"))
 
 # count the token frequency of each metaphor
@@ -9,7 +9,7 @@ n_metaphor <- marah %>%
   count(CM_BROADER, sort = TRUE, name = "n_token") %>% 
   mutate(n_perc_token = round(n_token/sum(n_token) * 100, digits = 2)) %>% 
   arrange(desc(n_perc_token))
-n_metaphor
+# n_metaphor
 
 joining_column <- colnames(n_metaphor)[1]
 
@@ -22,7 +22,7 @@ n_mapping <- marah %>%
   summarise(n_mapping = n_distinct(MAPPING), .groups = "drop") %>% 
   arrange(desc(n_mapping)) %>% 
   mutate(n_perc_mapping = round(n_mapping/sum(n_mapping) * 100, digits = 2))
-n_mapping
+# n_mapping
 
 # count the number of metaphorical lexical units/patterns by metaphor
 n_lu <- marah %>% 
@@ -31,12 +31,29 @@ n_lu <- marah %>%
   # summarise(n_lu = n_distinct(LU), .groups = "drop") %>% 
   mutate(n_perc_lu = round(n_lu/sum(n_lu) * 100, digits = 2)) %>% 
   arrange(desc(n_perc_lu))
-n_lu
+# n_lu
 
 # combining the data frame from the three measures (token, type, and mapping numbers) and calculate the salience score
 metaphor_salience <- n_metaphor %>% 
   left_join(n_mapping, by = joining_column) %>% 
   left_join(n_lu, by = joining_column) %>% 
-  mutate(salience_score = n_perc_token + n_perc_mapping + n_perc_lu) %>% 
-  arrange(desc(salience_score))
+  mutate(aggregate = n_perc_token + n_perc_mapping + n_perc_lu) %>% 
+  arrange(desc(aggregate))
 metaphor_salience %>% slice_head(n = 10)
+
+
+
+# read the metonymy data
+marah_metonymy <- read_tsv("data/token-based-approach-main.txt") %>% 
+  filter(USE == "metonymy")
+
+# metonymic salience analysis
+marah_metonymy_salience <- marah_metonymy %>% 
+  group_by(METONYMY) %>% 
+  summarise(n_token = n(), 
+            n_type = n_distinct(PATTERN),
+            .groups = "drop") %>% 
+  mutate(n_perc_token = round(n_token/sum(n_token) * 100, digits = 2),
+         n_perc_type = round(n_type/sum(n_type) * 100, digits = 2),
+         aggregate = n_perc_token + n_perc_type) %>% 
+  arrange(desc(n_token))
