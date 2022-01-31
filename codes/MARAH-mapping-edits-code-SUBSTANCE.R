31c4ac1a167c965966c8bb0944f9cb32f5868a44
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2022-01-31 14:49:26 +0800

    Add codes to edit the label of the fierce animal metaphor in the token based data.

--- codes/MARAH-mapping-edits-code-SUBSTANCE.R
+++ codes/MARAH-mapping-edits-code-SUBSTANCE.R
@@ -1,3 +1,4 @@
+# EDIT THE SUBSTANCE-related metaphors
 marah <- marah %>% 
   mutate(CM_BROADER = replace(CM_BROADER, CM_ROLEMAPPING == "anger is (un)mixed substance", "anger is (un)mixed substance"),
          MAP1 = replace(MAP1, CM_ROLEMAPPING == "anger is (un)mixed substance", "anger_substance"),
@@ -7,3 +8,7 @@ marah <- marah %>%
          MAP2 = replace(MAP2, CM_BROADER == "anger is substance in a container" & CM_ROLEMAPPING %in% c("anger is contained liquid", "anger is contained entity", "anger is located entity", "anger is moved object") & MAP2 %in% c("intensity_liquid-level", "intensity_fullness-degree"), "intensity_substance-fullness-level"),
          MAP3 = replace(MAP3, CM_BROADER == "anger is substance in a container" & CM_ROLEMAPPING %in% c("anger is contained liquid", "anger is contained entity", "anger is located entity", "anger is moved object") & MAP3 %in% c("intensity_liquid-level", "intensity_fullness-degree"), "intensity_substance-fullness-level"),
          CM_BROADER = replace(CM_BROADER, CM_ROLEMAPPING == "anger is heated fluid in a container", "anger is heated fluid in a container"))
+
+# EDIT OTHER METAPHORS
+marah <- marah %>% 
+  mutate(CM_BROADER = replace(CM_BROADER, CM_BROADER == "anger is captive (dangerous) animal", "anger is (fierce) captive animal"))
