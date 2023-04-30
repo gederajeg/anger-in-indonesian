e3aa434bd0ed92977e485dbed562a70ae099652a
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2023-04-30 10:20:38 +0800

    cosmetic: adjusting indentation and spacing.

--- codes/MARAH-mapping-edits-code-SUBSTANCE.R
+++ codes/MARAH-mapping-edits-code-SUBSTANCE.R
@@ -1,9 +1,14 @@
 # SUBSTANCE-related metaphors =======
 marah <- marah %>% 
-  mutate(CM_BROADER = replace(CM_BROADER, CM_ROLEMAPPING == "anger is (un)mixed substance", "anger is (un)mixed substance"),
-         MAP1 = replace(MAP1, CM_ROLEMAPPING == "anger is (un)mixed substance", "anger_substance"),
-         CM_BROADER = replace(CM_BROADER, CM_BROADER %in% c("anger is (heated) fluid in a container", 
-                                                            "anger is substance in a container") &
+  mutate(CM_BROADER = replace(CM_BROADER, 
+                              CM_ROLEMAPPING == "anger is (un)mixed substance", 
+                              "anger is (un)mixed substance"),
+         MAP1 = replace(MAP1, 
+                        CM_ROLEMAPPING == "anger is (un)mixed substance", 
+                        "anger_substance"),
+         CM_BROADER = replace(CM_BROADER, 
+                              CM_BROADER %in% c("anger is (heated) fluid in a container",
+                                                "anger is substance in a container") &
                                 CM_ROLEMAPPING %in% c("anger is contained liquid", 
                                                       "anger is contained entity", 
                                                       "anger is located entity", 
@@ -12,7 +17,8 @@ marah <- marah %>%
          CM_BROADER = replace(CM_BROADER, 
                               CM_ROLEMAPPING == "anger is pressing entity in a container", 
                               "anger is pressurised substance in a container"),
-         MAP1 = replace(MAP1, CM_BROADER == "anger is substance in a container" & 
+         MAP1 = replace(MAP1, 
+                        CM_BROADER == "anger is substance in a container" & 
                           CM_ROLEMAPPING %in% c("anger is contained liquid", 
                                                 "anger is contained entity", 
                                                 "anger is located entity", 
@@ -32,36 +38,62 @@ marah <- marah %>%
                                                 "anger is moved object") & 
                           MAP3 %in% c("intensity_liquid-level", "intensity_fullness-degree"), 
                         "intensity_substance-fullness-level"),
-         CM_BROADER = replace(CM_BROADER, CM_ROLEMAPPING == "anger is heated fluid in a container", "anger is heated fluid in a container"),
+         CM_BROADER = replace(CM_BROADER, 
+                              CM_ROLEMAPPING == "anger is heated fluid in a container", 
+                              "anger is heated fluid in a container"),
          
          ## node <w>mengandung</w> STATE
-         ROLEMAPPING_TD_IN_SFRAME = replace(ROLEMAPPING_TD_IN_SFRAME, MP == "node <w>mengandung</w> STATE", "substance"),
-         CM_ROLEMAPPING = replace(CM_ROLEMAPPING, MP == "node <w>mengandung</w> STATE", "anger is (un)mixed substance"),
-         CM_BROADER = replace(CM_BROADER, MP == "node <w>mengandung</w> STATE", "anger is (un)mixed substance"),
+         ROLEMAPPING_TD_IN_SFRAME = replace(ROLEMAPPING_TD_IN_SFRAME, 
+                                            MP == "node <w>mengandung</w> STATE", 
+                                            "substance"),
+         CM_ROLEMAPPING = replace(CM_ROLEMAPPING, 
+                                  MP == "node <w>mengandung</w> STATE", 
+                                  "anger is (un)mixed substance"),
+         CM_BROADER = replace(CM_BROADER, 
+                              MP == "node <w>mengandung</w> STATE", 
+                              "anger is (un)mixed substance"),
          MAP1 = replace(MAP1, MP == "node <w>mengandung</w> STATE", "anger_substance"),
          MAP2 = replace(MAP2, MP == "node <w>mengandung</w> STATE", "intensity_mixed-substance"),
          
          ## <w>mengendapkan</w> node (after the first review) 
-         MAP2 = replace(MAP2, MP == "<w>mengendapkan</w> node", "intense anger is the substance become hard"),
+         MAP2 = replace(MAP2, 
+                        MP == "<w>mengendapkan</w> node", 
+                        "intense anger is the substance become hard"),
          
          ## source frame of SUBMERGED ENTITY
-         SFRAME = replace(SFRAME, SFRAME == "cause upward motion", "caused upward motion"),
+         SFRAME = replace(SFRAME, 
+                          SFRAME == "cause upward motion", 
+                          "caused upward motion"),
          
          ## pattern 'menempatkan node ke dalam state' & 'mengarahkan node ke(pada)'
-         MP = replace(MP, MP == "<w>menempatkan node ke dalam</w> STATE", "<w>menempatkan</w> node <w>ke dalam</w> STATE"),
-         MP = replace(MP, MP == "EXP <w>mengarahkan node ke(pada)</w> TARGET", "EXP <w>mengarahkan</w> node <w>ke(pada)</w> TARGET")
+         MP = replace(MP, 
+                      MP == "<w>menempatkan node ke dalam</w> STATE", 
+                      "<w>menempatkan</w> node <w>ke dalam</w> STATE"),
+         MP = replace(MP, 
+                      MP == "EXP <w>mengarahkan node ke(pada)</w> TARGET", 
+                      "EXP <w>mengarahkan</w> node <w>ke(pada)</w> TARGET")
          )
 
 # OTHER METAPHORS ========
 marah <- marah %>% 
-  mutate(CM_BROADER = replace(CM_BROADER, CM_BROADER == "anger is captive (dangerous) animal", "anger is fierce, captive animal"),
-         CM_BROADER = replace(CM_BROADER, CM_BROADER == "anger is confining/impeding entity", "anger is confinement/impediment"),
-         CM_BROADER = replace(CM_BROADER, CM_BROADER == "anger is (destructive) natural force", "anger is natural force"),
+  mutate(CM_BROADER = replace(CM_BROADER, 
+                              CM_BROADER == "anger is captive (dangerous) animal", 
+                              "anger is fierce, captive animal"),
+         CM_BROADER = replace(CM_BROADER, 
+                              CM_BROADER == "anger is confining/impeding entity", 
+                              "anger is confinement/impediment"),
+         CM_BROADER = replace(CM_BROADER, 
+                              CM_BROADER == "anger is (destructive) natural force", 
+                              "anger is natural force"),
          CM_BROADER = replace(CM_BROADER, 
                               str_detect(CM_BROADER, "sence of object in some location"), 
                               "anger is (ab/pre)sence of an object"),
-         CM_BROADER = replace(CM_BROADER, str_detect(CM_BROADER, "is physical harm"), "anger is physical contact/harm"),
-         CM_BROADER = replace(CM_BROADER, CM_BROADER == "anger is sleeping entity", "anger is a sleeping organism"))
+         CM_BROADER = replace(CM_BROADER, 
+                              str_detect(CM_BROADER, "is physical harm"), 
+                              "anger is physical contact/harm"),
+         CM_BROADER = replace(CM_BROADER, 
+                              CM_BROADER == "anger is sleeping entity", 
+                              "anger is a sleeping organism"))
 
 # verticality scale
 marah <- marah %>% 
--- codes/MARAH-mapping-edits-code.R
+++ codes/MARAH-mapping-edits-code.R
@@ -2,14 +2,18 @@
 
 metaphor_tokenbased_mapping <- metaphor_tokenbased_mapping %>% 
   mutate(MAPPING = replace(MAPPING, 
-                           MAPPING %in% c("non-experiencer_fighter2", "experiencer_fighter2") & CM_BROADER == "anger is adversary", 
+                           MAPPING %in% c("non-experiencer_fighter2", "experiencer_fighter2") & 
+                             CM_BROADER == "anger is adversary", 
                            "experiencer-or-other-states_fighter2"),
          MAPPING = replace(MAPPING,
-                           CM_BROADER %in% c("anger is substance in a container", "anger is pressurised substance in a container") & 
-                             MAPPING %in% c("expressing anger is content going out", "expression of anger is expelling object"),
+                           CM_BROADER %in% c("anger is substance in a container", 
+                                             "anger is pressurised substance in a container") &
+                             MAPPING %in% c("expressing anger is content going out", 
+                                            "expression of anger is expelling object"),
                            "expressing anger is expelling/making contents out"),
          MAPPING = replace(MAPPING,
-                           CM_BROADER %in% c("anger is substance in a container", "anger is pressurised substance in a container") & 
+                           CM_BROADER %in% c("anger is substance in a container", 
+                                             "anger is pressurised substance in a container") & 
                              MAPPING %in% c("increased intensity is increased pressure", 
                                             "intense anger is pressing substance in a container"),
                            "increased intensity is increased pressure of the substance to the container"),
